b121457122f176400c5f98593dcc29a8b19836e6
eduardobonatto <edubonattogili@hotmail.com>
2022-08-10 19:55:05 -0300

    Atividade_01

---
+++ atividade_01/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Eduardo Bonatto Gilicsynski</h1>
+      </div>
+  );
+}
+
+export default App;
